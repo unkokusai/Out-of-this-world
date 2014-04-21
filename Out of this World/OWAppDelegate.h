c581714d444f5c6b509f17722c28a6ef62dd6959
Yasuyuki Pham <ypham@Yasuyukis-Mac-mini.local>
2014-04-21 01:25:52 -0700

    Initial Commit

---
+++ Out of this World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this World
+//
+//  Created by Yasuyuki Pham on 4/21/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
